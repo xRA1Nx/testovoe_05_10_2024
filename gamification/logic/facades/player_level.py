4a391bc9fb5ba6d0ca46914c199ef199168b0326
st.porhachevsky <st.porhachevsky@bestdoctor.ru>
2024-10-05 15:03:42 +0300

    TESTOVOE_05_10_2024: Rename function player_levels__csv_report_in_bytes

--- gamification/logic/facades/player_level.py
+++ gamification/logic/facades/player_level.py
@@ -7,9 +7,9 @@ from gamification.logic.interactors.player_levels_csv import player_levels__repo
 from gamification.models import PlayerLevel
 
 
-def player_levels__csv_report(*, player_levels: QuerySet[PlayerLevel] | None = None) -> bytes:
+def player_levels__csv_report_in_bytes(*, player_levels: QuerySet[PlayerLevel] | None = None) -> bytes:
     """
-    Получение отчета в формате .csv по Уровням игрока и вознаграждений за их прохождения.
+    Получение байтового представления отчета в формате csv по Уровням игрока и вознаграждений за их прохождения.
     """
     dto_list = player_levels__report_dtos(player_levels=player_levels)
     return player_levels__report_dtos_csv_to_bytes(dto_list=dto_list)
